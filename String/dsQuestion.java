f2cbb636e58892beaf2883d08095884dffd1c2ec
Tapan Kumar <tapankumar08521@gmail.com>
2024-08-08 17:52:43 +0530

    add code

---
+++ String/dsQuestion.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class dsQuestion {
+    public static void main(String [] args){
+        String str1="codequotient";
+        String str2="quotientcode";
+        String temp=str1+str2;
+        if(temp.contains(str2)){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+        
+
+    }  
+}
---
+++ StringComprision.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StringComprision {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str="";
+       for(int i=0; i<str.length(); i++){
+        
+       }
+    }
+    
+}
